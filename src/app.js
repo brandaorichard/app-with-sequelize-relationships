cf13cdeba3cc8b4ebc9ef235c80c914d42cee640
brandaorichard <rih.brandao@gmail.com>
2024-01-16 02:07:16 -0300

    feat: Create operations

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 
+const employee = require('./controllers/employee.controller');
+
 const app = express();
 app.use(express.json());
 
 app.get('/health/live', (req, res) => res.sendStatus(200));
+
+app.get('/employees', employee.getAll);
 
 module.exports = app;
--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -1,10 +1,8 @@
+// src/models/employee.model.js
+
 module.exports = (sequelize, DataTypes) => {
   const Employee = sequelize.define('Employee', {
-    id: { 
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     age: DataTypes.INTEGER,
