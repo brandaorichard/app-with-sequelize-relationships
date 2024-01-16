ec666aa550abfeabbd199211ce440b69bcf188b4
brandaorichard <rih.brandao@gmail.com>
2024-01-16 01:33:22 -0300

    feat: Create seeds

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const app = require('./app');
 
-app.listen(3001, () => {
-  console.log('App running on port 3001...');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
